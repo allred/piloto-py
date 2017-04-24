9e3fd3d4104dd6e25be4fbab79cc6d630071e276
Mike Allred <mikejallred@gmail.com>
2017-04-24 17:10:47 -0400

    Adding loader, other fixes.

--- db.py
+++ db.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 # run ./% --help
 import click
+import glob
+import json
+import os
 import peewee
+import re
 from playhouse.postgres_ext import *
 
 database = PostgresqlDatabase(
   'piloto',
 )
+
+dir_log_piloto = os.environ['HOME'] + "/m/rp3-piloto-1/log"
+
+# SCHEMA DEFINITIONS
 
 class BaseModel(Model):
   class Meta:
@@ -21,6 +29,8 @@ class Geolocator(BaseModel):
       (('data', 'tstamp'), True),
     )
 
+# UTILITY FUNCTIONS
+
 def create_tables():
   database.get_conn()
   database.create_tables([
@@ -32,6 +42,30 @@ def drop_tables():
   database.drop_tables([
     Geolocator,
   ], safe=True)
+
+def load_tables():
+  load_geolocator()
+
+def load_geolocator():
+  print("loading geolocator", end='')
+  database.get_conn()
+  index = 0
+  for file in glob.glob(dir_log_piloto + "/geolocator/*.log"):
+    for line in open(file, "r").readlines():
+      # there is some junk data, ensure it looks like json
+      if re.match("^\{", line) == None:
+        continue 
+      data = json.loads(line)
+      Geolocator.get_or_create(
+        data=line,
+        tstamp = data['time']
+      )
+      if index % 1000 == 0:
+        print('.', end='', flush=True)
+      index += 1
+  print("\n")
+
+# COMMANDS ###################################################################
 
 @click.group()
 def cli():
@@ -50,6 +84,11 @@ def drop():
 @cli.command()
 def list():
   click.echo('list goes here')
+
+@cli.command()
+def load():
+  #click.echo('loading tables')
+  load_tables()
 
 if __name__ == '__main__':
   cli()
