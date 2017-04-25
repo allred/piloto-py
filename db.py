75444c5122dbe55f71b9c40adcf4f2626af466cb
Mike Allred <mikejallred@gmail.com>
2017-04-24 22:39:47 -0400

    Adding table list.

--- db.py
+++ db.py
@@ -7,6 +7,7 @@ import json
 import os
 import peewee
 import re
+import sys
 from playhouse.postgres_ext import *
 
 database = PostgresqlDatabase(
@@ -34,7 +35,6 @@ class Geolocator(BaseModel):
   tstamp = DateTimeTZField()
   data = BinaryJSONField()
   class Meta:
-    database = database
     indexes = (
       (('data', 'tstamp'), True),
     )
@@ -54,6 +54,51 @@ def drop_tables():
   database.get_conn()
   database.drop_tables(tables, safe=True) 
 
+def list_tables():
+  c = database.get_conn().cursor() 
+  c.execute('''
+
+SELECT nspname || '.' || relname AS "relation",
+    pg_size_pretty(pg_relation_size(C.oid)) AS "size"
+  FROM pg_class C
+  LEFT JOIN pg_namespace N ON (N.oid = C.relnamespace)
+  WHERE nspname NOT IN ('pg_catalog', 'information_schema')
+  ORDER BY pg_relation_size(C.oid) DESC
+  LIMIT 20;
+
+/*
+    select relname, relpages
+    from pg_class
+    order by relpages desc
+    limit 20
+*/
+
+/*
+SELECT *, pg_size_pretty(total_bytes) AS total
+    , pg_size_pretty(index_bytes) AS INDEX
+    , pg_size_pretty(toast_bytes) AS toast
+    , pg_size_pretty(table_bytes) AS TABLE
+  FROM (
+  SELECT *, total_bytes-index_bytes-COALESCE(toast_bytes,0) AS table_bytes FROM (
+      SELECT c.oid,nspname AS table_schema, relname AS TABLE_NAME
+              , c.reltuples AS row_estimate
+              , pg_total_relation_size(c.oid) AS total_bytes
+              , pg_indexes_size(c.oid) AS index_bytes
+              , pg_total_relation_size(reltoastrelid) AS toast_bytes
+          FROM pg_class c
+          LEFT JOIN pg_namespace n ON n.oid = c.relnamespace
+          WHERE relkind = 'r'
+  ) a
+) a
+  ORDER BY total
+  -- LIMIT 2 
+;
+*/
+  ''')
+  print([desc[0] for desc in c.description])
+  for r in c:
+    print(r)
+
 def load_tables():
   database.get_conn()
   load_bluelog()
@@ -65,7 +110,7 @@ def progbar(i, every = 1):
 
 def load_bluelog():
   print("loading bluelog", end='')
-  re_skip = re.compile(".*Scan started.*|.*Scan ended.*|.*\x00.*")
+  re_skip = re.compile(".*Scan started on.*|.*Scan ended.*|.*\x00.*")
   for i, file in enumerate(glob.glob(dir_log_piloto + "/btoothlog/*.log")):
     progbar(i, 5)
     for line in open(file, "r").readlines():
@@ -95,6 +140,7 @@ def load_geolocator():
       )
   print("\n")
 
+
 # COMMANDS ###################################################################
 
 @click.group()
@@ -113,7 +159,8 @@ def drop():
 
 @cli.command()
 def list():
-  click.echo('list goes here')
+  #click.echo('listing tables')
+  list_tables()
 
 @cli.command()
 def load():
